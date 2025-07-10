471596f6e6a5dbbdb776ecd85654cc5f8668d73d
Marilaisanchez <marilaisanchez28gmail.com>
2025-07-09 19:59:33 -0600

    Actualización

---
+++ main.py
@@ -0,0 +1,11 @@
+a = int(input("Primer dígito:"))
+b = int(input("Segundo dígito:"))
+
+#Cálculos
+suma = a+b
+resta = a-b
+División = a/b
+
+#Resultados
+print("Suma total:", suma, "Resta total:", resta, "División", División)
+
